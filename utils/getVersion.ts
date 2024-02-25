2b868a90053da459ba50f3c09d15988ba9554fe5
yiliang114 <1204183885@qq.com>
2024-02-25 20:36:45 +0800

    wip

--- utils/getVersion.ts
+++ utils/getVersion.ts
@@ -1,4 +1,5 @@
 // https://github.com/microsoft/playwright/blob/v1.41.2/README.md
+// https://github.com/microsoft/vscode/blob/release/1.87/package.json
 
 // 1. 获取 vscode oss 所有形如 release/xxx 相关的分支列表。
 import { Octokit } from "octokit";
@@ -30,8 +31,47 @@ async function getBranches() {
   }
 }
 
-getBranches();
+// getBranches();
 
 // 2. 获取对应版本的 playwright-core 版本
+const url =
+  "https://github.com/microsoft/playwright/blob/v1.41.2/README.md?plain=1";
+
+async function getPlaywrightCoreVersion() {
+  const response = await fetch(url);
+  const text = await response.text();
+  console.log(
+    parseMarkdownTable(JSON.parse(text)?.payload?.blob?.rawLines?.join("\n"))
+  );
+}
+
+function parseMarkdownTable(markdown) {
+  console.log(markdown);
+  const regex = /\|(.+)\|\n\|([\s:-]+)\|\n((?:\|.+?\|\n)+)/g;
+  const headers = regex
+    .exec(markdown)?.[1]
+    ?.split("|")
+    .map((header) => header.trim())
+    .slice(1, -1);
+  const rows = [];
+  let match;
+  while ((match = regex.exec(markdown)) !== null) {
+    const cells = match[3].split("|").map((cell) => cell.trim());
+    const row = cells.slice(1, -1).map((cell) => {
+      if (cell === ":white_check_mark:") {
+        return true;
+      } else if (cell === ":x:") {
+        return false;
+      } else {
+        return cell;
+      }
+    });
+    rows.push(row);
+  }
+  const data = { headers, rows };
+  return data;
+}
+
+getPlaywrightCoreVersion();
 
 // 3. 获取 playwright 对应版本支持的浏览器列表
